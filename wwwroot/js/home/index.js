3069e78bc043c4769274d5e3d012729ccb53c233
Frederick Aquino <xnox.45@hotmail.com>
2024-04-03 21:38:49 -0300

    Ajuste para baixar corretamente

--- wwwroot/js/home/index.js
+++ wwwroot/js/home/index.js
@@ -120,43 +120,21 @@ $(document).ready(function () {
     $("#downloadButton").click(function () {
         var formData = new FormData($("#filterForm")[0]);
 
-        //$.ajax({
-        //    url: "/Home/ExportarParaExcel",
-        //    type: "POST",
-        //    data: formData,
-        //    contentType: false,
-        //    processData: false,
-        //    success: function (data) {
-        //        var blob = new Blob([data.fileContents], { type: 'application/octet-stream' });
-
-        //        // Usa o FileSaver.js para baixar o arquivo
-        //        saveAs(blob, data.fileDownloadName);
-        //    },
-        //    error: function (xhr, status, error) {
-        //        // Trata erros
-        //        console.error(xhr.responseText);
-        //        console.error(error);
-        //    }
-        //});
-
-        //$.ajax({
-        //    url: '/Home/ExportarParaExcel',
-        //    type: 'POST',
-        //    data: formData,
-        //    xhrFields: {
-        //        responseType: 'blob'
-        //    },
-        //    success: function (data) {
-        //        var blob = new Blob([data]);
-        //        var link = document.createElement('a');
-        //        link.href = window.URL.createObjectURL(blob);
-        //        link.download = 'Clientes.xlsx';
-        //        link.click();
-        //    },
-        //    error: function (xhr, status, error) {
-        //        console.error(xhr.responseText);
-        //    }
-        //});
+        $.ajax({
+            url: "/Home/ExportarParaExcel",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            async: false,
+            success: function (data) {
+            },
+            error: function (xhr, status, error) {
+                // Trata erros
+                console.error(xhr.responseText);
+                console.error(error);
+            }
+        });
 
         location.href = 'Home/DownloadCSV';
 
